e085f4a098809a234e7d635e01cafa1a21582ecf
Johannes <johannes.barjak@gmail.com>
2024-06-10 15:56:59 -0500

    Add initial types and board

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,24 @@
 module Main where
 
-import qualified LambdaChess (someFunc)
+import LambdaChess
+import Data.Map qualified as Map
 
 main :: IO ()
-main = do
-  putStrLn "Hello, Haskell!"
-  LambdaChess.someFunc
+main = print $ showBoard standardBoard
+
+showBoard :: Board -> [String]
+showBoard board = do
+  rows <- enumFrom A
+  pure $ do
+    cols <- enumFrom A
+
+    let piece = Map.lookup (Square rows cols) board
+    pure $ maybe ' ' pieceToChar piece
+
+pieceToChar :: Piece -> Char
+pieceToChar (Piece King _)   = 'K'
+pieceToChar (Piece Queen _)  = 'Q'
+pieceToChar (Piece Rook _)   = 'R'
+pieceToChar (Piece Bishop _) = 'B'
+pieceToChar (Piece Knight _) = 'N'
+pieceToChar (Piece Pawn _)   = 'P'
--- src/LambdaChess.hs
+++ src/LambdaChess.hs
@@ -1,4 +1,52 @@
-module LambdaChess (someFunc) where
+module LambdaChess
+  ( Piece(..)
+  , PieceType(..)
+  , Color(..)
+  , Square(..)
+  , Coord(..)
+  , Board
+  , standardBoard
+  ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Map qualified as Map
+import Data.Map (Map)
+
+type Board = Map Square Piece
+data Square = Square {-# UNPACK #-} !Coord !Coord
+  deriving (Eq, Ord)
+
+data Piece = Piece PieceType Color
+
+data PieceType
+  = King
+  | Queen
+  | Rook
+  | Bishop
+  | Knight
+  | Pawn
+
+data Coord
+  = A | B
+  | C | D
+  | E | F
+  | G | H
+  deriving (Eq, Ord, Enum)
+
+data Color = White | Black
+
+standardBoard :: Board
+standardBoard = Map.fromList
+  $  standardPlacement White A
+  ++ pawnRow White B
+  ++ standardPlacement Black H
+  ++ pawnRow Black G
+
+standardPlacement :: Color -> Coord -> [(Square, Piece)]
+standardPlacement col coord = zip coordinates pieces
+  where coordinates = map (Square coord) (enumFrom A)
+
+        pieces = map (`Piece` col) (leftPieces ++ [Queen, King] ++ reverse leftPieces)
+        leftPieces = [Rook, Knight, Bishop]
+
+pawnRow :: Color -> Coord -> [(Square, Piece)]
+pawnRow c coord = map ((, Piece Pawn c) . Square coord) (enumFrom A)
