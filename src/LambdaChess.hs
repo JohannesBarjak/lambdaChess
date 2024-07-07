7c81778b0b56290e04dfc4c3f07327e34e66221f
Johannes <johannes.barjak@gmail.com>
2024-07-07 10:49:12 -0500

    Add rook moves

--- src/LambdaChess.hs
+++ src/LambdaChess.hs
@@ -20,11 +20,13 @@ moves bd = flip (maybe []) (extract bd) \case
   (Piece Pawn White) -> catMaybes [sqUp (pos bd)]
   (Piece Pawn Black) -> catMaybes [sqDown (pos bd)]
   (Piece Bishop   _) -> concat . mapM boardLine bishopMoves $ pos bd
+  (Piece Rook     _) -> concat . mapM boardLine rookMoves $ pos bd
   _ -> []
 
   where boardLine f = unfoldr (fmap (join (,)) . f)
 
         bishopMoves = [sqUp >=> sqRight, sqUp >=> sqLeft, sqDown >=> sqRight, sqDown >=> sqLeft]
+        rookMoves   = [sqUp, sqDown, sqLeft, sqRight]
 
 move :: Square -> Chessboard -> Chessboard
 move target bd = seek target bd&bdSel .~ extract bd&seek (pos bd)&bdSel .~ Nothing
